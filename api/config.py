bd66e1bb3fb1d8cb5e44c97eadb9a329e6792b5c
camilo campos <ccampos@technonest.cl>
2024-08-05 15:46:30 -0400

    cambios

--- api/config.py
+++ api/config.py
@@ -1,10 +1,13 @@
-import os
+
+
 
 class ServerConfig:
+
     SECRET_KEY = 'your_secret_key'
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Directorio actual de config.py
-    CERT_PATH = os.path.join(BASE_DIR, 'certificate.crt')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://ibm_cloud_9467e667_81b9_4619_8b02_96c77cd1b716:08f87cd8eebe85554b0e3e2ee555fbe6c05e92fccf7364c3e61d556b9740cd19@8c45c960-1ec9-46fb-b8b4-d1d4413c4336.6131b73286f34215871dfad7254b4f7d.databases.appdomain.cloud:30596/ibmclouddb?sslmode=verify-full&sslrootcert={CERT_PATH}'
+    
+    
+    
+    SQLALCHEMY_DATABASE_URI ='postgresql://ibm_cloud_9467e667_81b9_4619_8b02_96c77cd1b716:08f87cd8eebe85554b0e3e2ee555fbe6c05e92fccf7364c3e61d556b9740cd19@8c45c960-1ec9-46fb-b8b4-d1d4413c4336.6131b73286f34215871dfad7254b4f7d.databases.appdomain.cloud:30596/ibmclouddb?sslmode=verify-full&sslrootcert=certificate.crt'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
