68beb002a363262ce6d0d814d69715a66bc0399f
camilo campos <ccampos@technonest.cl>
2024-08-05 15:58:58 -0400

    cambios en dockerfile y config.py

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-#Deriving the base image
+# Deriving the base image
 FROM python:3.10-slim-bullseye
 
 # Crear un entorno virtual
 RUN python -m venv /opt/env
 
 # Activar el entorno virtual
-ENV PATH="/opt/venv/bin:$PATH"
+ENV PATH="/opt/env/bin:$PATH"
 
 # Establecer el directorio de trabajo en el contenedor
 WORKDIR /usr/app/src
@@ -13,10 +13,11 @@ WORKDIR /usr/app/src
 # Copiar los archivos necesarios al contenedor
 COPY requirements.txt .
 COPY api/ ./api/
-
+COPY api/certificate.crt ./certificate.crt
 
 # Instalar las dependencias
 RUN pip install --no-cache -r requirements.txt
 
 # Comando para ejecutar la aplicación
-CMD [ "python", "./api/app.py"]
+CMD ["python", "./api/app.py"]
+
--- api/config.py
+++ api/config.py
@@ -4,10 +4,7 @@
 class ServerConfig:
 
     SECRET_KEY = 'your_secret_key'
-    
-    
-    
-    SQLALCHEMY_DATABASE_URI ='postgresql://ibm_cloud_9467e667_81b9_4619_8b02_96c77cd1b716:08f87cd8eebe85554b0e3e2ee555fbe6c05e92fccf7364c3e61d556b9740cd19@8c45c960-1ec9-46fb-b8b4-d1d4413c4336.6131b73286f34215871dfad7254b4f7d.databases.appdomain.cloud:30596/ibmclouddb?sslmode=verify-full&sslrootcert=certificate.crt'
+    SQLALCHEMY_DATABASE_URI ='postgresql://ibm_cloud_9467e667_81b9_4619_8b02_96c77cd1b716:08f87cd8eebe85554b0e3e2ee555fbe6c05e92fccf7364c3e61d556b9740cd19@8c45c960-1ec9-46fb-b8b4-d1d4413c4336.6131b73286f34215871dfad7254b4f7d.databases.appdomain.cloud:30596/ibmclouddb?sslmode=verify-full&sslrootcert=/usr/app/src/certificate.crt'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
